44ca8f15ef3d3cce628b7287f11574e21f17ed61
Sunghun Park <sunghun.josephp@gmail.com>
2022-04-26 22:54:09 -0700

    Style show page buttons and input

--- models/seed/gameSeed.js
+++ models/seed/gameSeed.js
@@ -1,26 +1,34 @@
 module.exports = [
 	{
-		name: "Batman",
-		desc: "A bean",
-		img: "https://dummyimage.com/300x200/5c3f2f/000000.png&text=Bean",
+		name: "Batman: Arkham Knight",
+		desc: "Batman: Arkham Knight is an action-adventure game set within an open world Gotham City, which can be explored freely by the player from the beginning of the game, allowing them to travel seamlessly anywhere within its boundaries.",
+		img: "https://gamefaqs.gamespot.com/a/box/0/9/0/362090_front.jpg",
 		price: 5.99,
 		qty: 40,
-		tags: ['action', 'adventure']
+		tags: ['action', 'adventure', 'open-world']
 	},
 	{
-		name: "Border",
-		desc: "A border",
-		img: "https://dummyimage.com/300x200/e303e3/000000.png&text=border",
+		name: "Elden Ring",
+		desc: "Elden Ring is an action role-playing game played in a third-person perspective with gameplay focusing on combat and exploration",
+		img: "https://gamefaqs.gamespot.com/a/box/9/5/6/824956_front.jpg",
 		price: 15.99,
 		qty: 20,
-		tags: ['thriller', 'heart-warming']
+		tags: ['action', 'heart-warming', 'rpg']
 	},
 	{
-		name: "Asparagus",
-		desc: "A asparagus",
-		img: "https://dummyimage.com/300x200/53b089/000000.png&text=asparagus",
+		name: "Destiny 2",
+		desc: "Destiny 2 is a first-person shooter game that incorporates role-playing and massively multiplayer online game (MMO)",
+		img: "https://gamefaqs.gamespot.com/a/box/4/2/0/628420_front.jpg",
 		price: 99.99,
-		qty: 1,
-		tags: ['action', 'story']
+		qty: 400,
+		tags: ['action', 'mmo']
+	},
+	{
+		name: "Old School Rune Scape",
+		desc: "The world's largest and most popular MMORPG, Old School RuneScape has been played by over 260 million players since the 2001 release of RuneScape.",
+		img: "https://gamefaqs.gamespot.com/a/box/7/3/5/784735_front.jpg",
+		price:15.09,
+		qty: 123,
+		tags: ['mmo', 'role-playing']
 	}
 ];
